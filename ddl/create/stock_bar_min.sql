6acd2558ee8ea721447800e77463d74b4dbcae72
tau <ttru702@gmail.com>
2022-07-25 18:54:38 +0900

    add ddl stock_bar

---
+++ ddl/create/stock_bar_min.sql
@@ -0,0 +1,13 @@
+CREATE TABLE public.stock_bar (
+	"timestamp" timestamptz NOT NULL,
+	symbol bpchar(8) NOT NULL,
+	time_scale bpchar(8) NOT NULL,
+	"open" numeric NOT NULL,
+	high numeric NOT NULL,
+	low numeric NOT NULL,
+	"close" numeric NOT NULL,
+	volume int4 NOT NULL,
+	trade_count int4 NOT NULL,
+	vwap numeric NOT NULL,
+	CONSTRAINT stock_bar_pk PRIMARY KEY ("timestamp", symbol, time_scale)
+);
