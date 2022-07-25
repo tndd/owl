f30bdc1e2398ef2f74aebb81c8dc12d6ea9de6cc
tau <ttru702@gmail.com>
2022-07-25 19:55:00 +0900

    update test

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
+import psycopg2
+
 from dotenv import load_dotenv
 from alpaca_trade_api.rest import REST, TimeFrame
-import psycopg2
 from sqlalchemy import create_engine
+from time import time
 
 load_dotenv()
 api = REST()
@@ -21,12 +23,14 @@ with open('tickers_dow30.txt', 'r') as f:
     tickers = [t.rstrip('\n') for t in f.readlines()]
 
 
-for ticker in tickers:
-    df = api.get_bars(ticker, TimeFrame.Minute, "2022-07-22", "2022-07-24", adjustment='raw').df
+print('idx | ticker | time_dl | time_store')
+for i, ticker in enumerate(tickers):
+    t_start = time()
+    df = api.get_bars(ticker, TimeFrame.Minute, "2012-07-24", "2022-07-24", adjustment='raw').df
     df.insert(0, 'time_scale', TimeFrame.Minute.value)
     df.insert(0, 'symbol', ticker)
+    t_end_get_df = time()
     df.to_sql('stock_bar', con=engine, if_exists='append')
-    print(f'Complete: {ticker}')
-    print(df)
-    exit()
+    t_end_store_db = time()
+    print(f'{i}\t{ticker}\t{t_end_get_df - t_start}\t{t_end_store_db - t_start}')
 
