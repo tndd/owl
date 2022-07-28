52dad3e3529ea0cfd2172fd043dc236d9655717d
tau <ttru702@gmail.com>
2022-07-28 22:35:55 +0900

    update ddl

--- repository/sql/create/stock_bar.sql
+++ repository/sql/create/stock_bar.sql
@@ -1,14 +1,13 @@
-CREATE TABLE public.stock_bar (
-	"timestamp" timestamptz NOT NULL,
-	symbol bpchar(8) NOT NULL,
-	time_scale bpchar(8) NOT NULL,
-	"open" numeric NOT NULL,
-	high numeric NOT NULL,
-	low numeric NOT NULL,
-	"close" numeric NOT NULL,
-	volume int4 NOT NULL,
-	trade_count int4 NOT NULL,
-	vwap numeric NOT NULL,
-	CONSTRAINT stock_bar_pk PRIMARY KEY ("timestamp", symbol, time_scale)
-);
-CREATE INDEX stock_bar_symbol_idx ON public.stock_bar USING btree (symbol, time_scale);
+CREATE TABLE `alpaca_hist_bar` (
+  `symbol` varchar(64) NOT NULL,
+  `time_scale` varchar(64) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `open` decimal(12,6) NOT NULL,
+  `high` decimal(12,6) NOT NULL,
+  `low` decimal(12,6) NOT NULL,
+  `close` decimal(12,6) NOT NULL,
+  `volume` int unsigned NOT NULL,
+  `trade_count` int unsigned NOT NULL,
+  `vwap` decimal(12,6) NOT NULL,
+  PRIMARY KEY (`symbol`,`time_scale`,`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
