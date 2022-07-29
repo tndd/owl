ccfc974242e374158cb236189149ac73f25e0506
tau <ttru702@gmail.com>
2022-07-29 17:25:44 +0900

    add load_query

--- repository/broker.py
+++ repository/broker.py
@@ -9,21 +9,28 @@ from sqlalchemy.engine import Engine
 
 @dataclass
 class Broker:
+    # Database
     user: str = 'root'
     password: str = 'password'
     host: str = 'localhost'
     port: int = 3306
     database: str = 'sage_owl'
+    # SQL
+    pwd: str = Path(__file__).resolve().parent
 
-    @staticmethod
-    def load_tickers_from_file() -> List[str]:
+    def load_tickers_from_file(self) -> List[str]:
         tickers = []
-        f_cd = Path(__file__).resolve().parent
-        paths = glob(f'{f_cd}/tickers/*.txt')
+        paths = glob(f'{self.pwd}/tickers/*.txt')
         for path in paths:
             with open(path, 'r') as f:
                 tickers.extend([t.rstrip('\n') for t in f.readlines()])
         return tickers
+
+    def load_query(self, group: str, command: str, name: str) -> str:
+        path = f'{self.pwd}/sql/{group}/{command}/{name}.sql'
+        with open(path, 'r') as f:
+            query = f.read()
+        return query
 
     def get_engine(self) -> Engine:
         return create_engine(
