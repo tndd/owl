ddb33c617b80a5f724157dce14b24c4acb0cd55d
tau <ttru702@gmail.com>
2022-08-18 03:24:23 +0900

    add func test_create_tbl_hist_bar

--- tests/repository/test_historical_bar_alp.py
+++ tests/repository/test_historical_bar_alp.py
@@ -1,22 +1,58 @@
+import re
+
 from repository import RepositoryHistoricalBarAlp
 from repository.resource import BrokerDB
-from tests.mock_data.broker import BrokerMockData, DataGroup
+
+TEST_DB_NAME = '__test_sage_owl'
 
 
-def get_test_repository() -> RepositoryHistoricalBarAlp:
-    bkr_db = BrokerDB(database='__test_sage_owl')
+def get_repository_for_test(database: str = TEST_DB_NAME) -> RepositoryHistoricalBarAlp:
+    bkr_db = BrokerDB(database=database)
     return RepositoryHistoricalBarAlp(bkr_db=bkr_db)
 
 
-def test_store_hist_bar():
-    rp = get_test_repository()
-    df = BrokerMockData().load_data(DataGroup.HIST_BAR, 'AAPL_1Day')
-    print(df)
-    # rp.store_hist_bar(df)
+def drop_table_hist_bar(rp_hist_bar: RepositoryHistoricalBarAlp) -> None:
+    pattern = r'__test*'
+    db_name = rp_hist_bar.bkr_db.database
+    if not re.match(pattern, db_name):
+        raise Exception(f'This method can only be called on "{pattern}" database.')
+    query = f'drop table if exists {rp_hist_bar.tbl_hist_bar};'
+    rp_hist_bar.bkr_db.execute(query)
+
+
+def test_create_tbl_hist_bar() -> None:
+    rp = get_repository_for_test()
+    # clean table
+    drop_table_hist_bar(rp)
+    # create table
+    rp.create_tbl_hist_bar()
+    # test create_tbl_hist_bar
+    expd_scheme = [
+        ('symbol', b'varchar(64)', 'NO', 'PRI', None, ''),
+        ('timeframe', b'varchar(64)', 'NO', 'PRI', None, ''),
+        ('timestamp', b'datetime', 'NO', 'PRI', None, ''),
+        ('open', b'decimal(12,6)', 'NO', '', None, ''),
+        ('high', b'decimal(12,6)', 'NO', '', None, ''),
+        ('low', b'decimal(12,6)', 'NO', '', None, ''),
+        ('close', b'decimal(12,6)', 'NO', '', None, ''),
+        ('volume', b'int unsigned', 'NO', '', None, ''),
+        ('trade_count', b'int unsigned', 'NO', '', None, ''),
+        ('vwap', b'decimal(12,6)', 'NO', '', None, '')
+    ]
+    rp.bkr_db.cur.execute(f'DESC {rp.tbl_hist_bar};')
+    assert expd_scheme == rp.bkr_db.cur.fetchall()
+
+
+
+# def test_store_hist_bar():
+#     rp = get_repository_for_test()
+#     df = BrokerMockData().load_data(DataGroup.HIST_BAR, 'AAPL_1Day')
+#     print(df)
+#     rp.store_hist_bar(df)
 
 
 def main() -> None:
-    test_store_hist_bar()
+    test_create_tbl_hist_bar()
 
 
 if __name__ == '__main__':
